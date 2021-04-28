4c04add6211db96bd59e5ed979ddc98863327dd7
Joey Steigelman <joey.steigelman@gmail.com>
2021-04-28 15:15:27 +0200

    Added login/sign up screen. Set up Cocoapods. Added Parse client.

---
+++ Wordbuzz/wordDefinitionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  wordDefinitionViewController.swift
+//  Wordbuzz
+//
+//  Created by Joey Steigelman on 4/28/21.
+//
+
+import UIKit
+
+class wordDefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

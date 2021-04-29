1bc0ac39a931ea8eb10d29b856c93e68cdbaa6ca
Chizy <chizy7@gmail.com>
2021-04-29 13:50:08 -0400

    Test commit works

--- Wordbuzz/wordDefinitionViewController.swift
+++ Wordbuzz/wordDefinitionViewController.swift
@@ -13,7 +13,6 @@ class WordDefinitionViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print ("Hello World!")
     }
     
 

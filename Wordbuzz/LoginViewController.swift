85ff5618e7b95bfcd7b1c52a02b1be12a56668b0
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-04-28 23:07:07 -0400

    Test commit

--- Wordbuzz/LoginViewController.swift
+++ Wordbuzz/LoginViewController.swift
@@ -33,7 +33,7 @@ class LoginViewController: UIViewController {
             if user != nil {
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
             } else {
-                print("Error: \(error?.localizedDescription)")
+                print("Error: \(error?.localizedDescription ?? "")")
             }
         }
     }
@@ -48,7 +48,7 @@ class LoginViewController: UIViewController {
             if success {
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
             } else {
-                print("Error: \(error?.localizedDescription)")
+                print("Error: \(error?.localizedDescription ?? "")")
             }
         }
     }
--- Wordbuzz/wordDefinitionViewController.swift
+++ Wordbuzz/wordDefinitionViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class wordDefinitionViewController: UIViewController {
+class WordDefinitionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

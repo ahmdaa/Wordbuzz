e04849aaa79f353cfbced4ee164b07df06a61b14
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-05-01 16:55:55 -0400

    Add API request method for getting a random word from WordsAPI

--- Wordbuzz/wordDefinitionViewController.swift
+++ Wordbuzz/wordDefinitionViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import Foundation
 import Parse
 
 class WordDefinitionViewController: UIViewController {
+    
+    var randomWord = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        getRandomWord()
         // Do any additional setup after loading the view.
     }
     
+    // MARK:- Network Requests
     @IBAction func onLogout(_ sender: Any) {
         PFUser.logOut()
         
@@ -28,6 +33,34 @@ class WordDefinitionViewController: UIViewController {
         delegate.window?.rootViewController = loginViewController
     }
     
+    // MARK:- API
+    func getRandomWord() {
+        let headers = [
+            "x-rapidapi-key": "a10993a051msh078390884b6556fp17dfdfjsn6ef5bb0fb17f",
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+        ]
+
+        let url = URL(string: "https://wordsapiv1.p.rapidapi.com/words/?random=true")!
+        var request = URLRequest(url: url,
+                                 cachePolicy: .useProtocolCachePolicy,
+                                 timeoutInterval: 10.0)
+        
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+               print(error.localizedDescription)
+            } else if let data = data {
+               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+               
+               print(dataDictionary)
+            }
+        }
+        dataTask.resume()
+    }
     /*
     // MARK: - Navigation
 

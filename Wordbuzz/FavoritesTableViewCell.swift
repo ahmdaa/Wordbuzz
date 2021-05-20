354a0a4e1e9dadaf41c87f3d2f4f09690a6e630e
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-05-20 16:35:03 -0400

    Set up favorites view and view controller with prototype cell to display words

---
+++ Wordbuzz/FavoritesTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavoritesTableViewCell.swift
+//  Wordbuzz
+//
+//  Created by Ahmed Abdalla on 5/20/21.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var wordLabel: UILabel!
+
+}

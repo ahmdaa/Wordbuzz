99c7d03a69eca67dfd28a6c125f9f4e6d4b74b31
Chizy <chizy7@gmail.com>
2021-05-21 22:48:44 -0400

    Local notifications

--- Wordbuzz/SettingsViewController.swift
+++ Wordbuzz/SettingsViewController.swift
@@ -6,13 +6,46 @@
 //
 
 import UIKit
+import UserNotifications
 
 class SettingsViewController: UIViewController {
 
+    @IBAction func notiswitch(_ sender: Any) {
+        print ("Hi!")
+        // Write code here for turning on and off the notification switch and add another outlet for switchoff
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Asking for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            // Code later to let user know that if they deny notification that they can grant it later by going to settings
+        }
+        
+        // Creating notification content
+        let content = UNMutableNotificationContent()
+        content.title = "I am a notification!"
+        content.body = "Check me out!"
+        
+        // Creating the notification trigger
+        let date = Date().addingTimeInterval(5)
+        
+        let dateComponent = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date, to: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+        
+        // Creating the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // Registering the request with notification center
+        center.add(request) {(error) in
+            // check the error parameter and handle any errors
+        }
     }
     
 

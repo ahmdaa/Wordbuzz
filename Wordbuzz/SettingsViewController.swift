5e1cdff727b1f59b95f0c4868c7c9e4b8c95ce5d
Chizy <chizy7@gmail.com>
2021-05-28 02:31:06 -0400

    Constraints added and notifications 98% setup in the settings screen.

--- Wordbuzz/SettingsViewController.swift
+++ Wordbuzz/SettingsViewController.swift
@@ -10,9 +10,30 @@ import UserNotifications
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var switchout: UISwitch!
     @IBAction func notiswitch(_ sender: Any) {
         print ("Hi!")
-        // Write code here for turning on and off the notification switch and add another outlet for switchoff
+        // Turning notification switch on and off
+//        if switchout.isOn
+//        {
+//            if #available(iOS 10.0, *), list.isEmpty == false { //Fix error later
+//
+//                let content = UNMutableNotificationContent()
+//                content.title = "You have to keep up with your streak!"
+//                content.subtitle = ""
+//                content.body = "Launch app to keep up with learning"
+//                let alarmTime = Date().addingTimeInterval(60)
+//                let components = Calendar.current.dateComponents([.weekday, .hour, .minute], from: alarmTime)
+//                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+//                let request = UNNotificationRequest(identifier: "taskreminder", content: content, trigger: trigger)
+//                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+//            } else {
+//                print("hello")
+//            }
+//        } else {
+//            UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+//        }
+//        self.saveSwitchesStates() //Fix error later
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +43,13 @@ class SettingsViewController: UIViewController {
         // Asking for permission
         let center = UNUserNotificationCenter.current()
         
+        // Letting user know that if they deny notification that they can grant it later by going to settings
         center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
-            // Code later to let user know that if they deny notification that they can grant it later by going to settings
+            if granted {
+                print("Notifications permission granted")
+            } else {
+                print("Notification permission denied because: \(error?.localizedDescription).")
+            }
         }
         
         // Creating notification content

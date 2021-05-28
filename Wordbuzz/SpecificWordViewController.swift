57047c551cb16a52777b76d51ced6ee2917db711
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-05-27 20:34:44 -0400

    Add specific word view to show a favorite word's details

---
+++ Wordbuzz/SpecificWordViewController.swift
@@ -0,0 +1,148 @@
+//
+//  SpecificWordViewController.swift
+//  Wordbuzz
+//
+//  Created by Ahmed Abdalla on 5/27/21.
+//
+
+import UIKit
+
+class SpecificWordViewController: UIViewController {
+    
+    @IBOutlet weak var frequencyLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var examplesLabel: UILabel!
+    
+    var specificWord = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        getWord()
+        // Do any additional setup after loading the view.
+    }
+    
+    func getWord() {
+        print("Making request..")
+        let headers = [
+            "x-rapidapi-key": "a10993a051msh078390884b6556fp17dfdfjsn6ef5bb0fb17f",
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+        ]
+        
+        let urlString = String(format: "https://wordsapiv1.p.rapidapi.com/words/%@", specificWord)
+        
+        if let url = URL(string: urlString) {
+            
+            var request = URLRequest(url: url,
+                                     cachePolicy: .useProtocolCachePolicy,
+                                     timeoutInterval: 10.0)
+            
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
+
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: request) { (data, response, error) in
+                // This will run when the network request returns
+                if let error = error {
+                   print(error.localizedDescription)
+                } else if let data = data {
+                   let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    
+                    // print(dataDictionary)
+                    
+                    DispatchQueue.main.async {
+                        
+                        if let word = dataDictionary["word"] as? String {
+                            self.wordLabel.text = word
+                            // self.saveSeenWord(seen: word)
+                        }
+                        
+                        if let freq = dataDictionary["frequency"] as? Double {
+                            self.frequencyLabel.text = String(freq)
+                        }
+                        
+                        //print(dataDictionary["results"][0] ?? "")
+                        //print(dataDictionary["syllables"] ?? "")
+                        
+                        // Get all definitions if any are available
+                        if let definitions = dataDictionary["results"] as? [[String: Any]] {
+                            
+                            var definitionList = "" // Multi-line string for definitions
+                            var examplesList = "" // Multi-line string for examples
+                            var definitionIndex = 1 // Index for each definition
+                            var exampleIndex = 1 // Index for each example
+                            
+                            for definition in definitions {
+                                if let definitionText = definition["definition"] as? String {
+                                    definitionList += String(definitionIndex) + ". " + definitionText + "\n"
+                                    definitionIndex += 1
+                                }
+                                
+                                if let examples = definition["examples"] as? [String] {
+                                    examplesList += String(exampleIndex) + ". "
+                                    for example in examples {
+                                        examplesList += "\"" + example + "\"\n"
+                                    }
+                                    exampleIndex += 1
+                                } else {
+                                    exampleIndex += 1
+                                }
+                                
+                            }
+                            
+                            self.definitionLabel.text = definitionList
+                            
+                            if examplesList == "" {
+                                self.examplesLabel.text = "No examples found"
+                            } else {
+                                self.examplesLabel.text = examplesList
+                            }
+                            
+                            // print(definitionList)
+                            
+                            // Set the word's definition label text
+                            /* if let definitionText = definition["definition"] as? String {
+                                self.definitionLabel.text = definitionText
+                            } else {
+                                self.definitionLabel.text = "No definition found"
+                            } */
+                            
+                            // Set the word's examples label text
+                            /* if let examples = firstDefinition["examples"] as? [String] {
+                                var examplesText = ""
+                                for example in examples {
+                                    examplesText += "\"" + example + "\"\n"
+                                }
+
+                                self.examplesLabel.text = examplesText
+                            } else {
+                                self.examplesLabel.text = "No examples found"
+                            } */
+                        } else {
+                            self.definitionLabel.text = "No definition found"
+                            self.examplesLabel.text = "No examples found"
+                        }
+                    }
+                    
+                    
+                }
+            }
+            dataTask.resume()
+        } else {
+            print("Invalid URL")
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
